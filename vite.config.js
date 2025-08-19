c99103aa291883c3e8a99e415e873cfd9ee6bae2
desuraj <suraj.deo1160@gmail.com>
2025-08-19 13:00:05 +0530

    branch wise deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const branch = process.env.BRANCH_NAME || 'main'
+
+const repoName =
+  branch === 'main'
+    ? 'e-commerce-web-saas'
+    : `e-commerce-web-saas/${branch}`
 // Replace with your repo name
 export default defineConfig({
-  base: '/e-commerce-web-saas/',
+  base: `/${repoName}/`,
   plugins: [react()],
 })
